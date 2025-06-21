a34b6ce0321c934fcb837e439ddbfeeb0d8f0218
mohdrafey1 <mohdrafey600@gmail.com>
2025-06-21 15:54:58 +0530

    build fail error

--- src/app/blog/post/[slug]/page.tsx
+++ src/app/blog/post/[slug]/page.tsx
@@ -334,57 +334,59 @@ function BlogPostComponent({
     );
 }
 
-export async function generateMetadata(props: { params: tParams }) {
-    const { slug } = await props.params;
-    const blogId = slug[1];
-    const postRes = await fetch(`${api.blog.allBlogs}/${blogId}`, {
-        cache: "no-store",
-        headers: { "Content-Type": "application/json" },
-    });
+//https://chatgpt.com/share/68568825-d470-800e-a224-d65720f59df5 // need to correct it show build not get failed
 
-    if (!postRes.ok) {
-        return {
-            title: "Blog Post Not Found",
-            description: "The requested blog post could not be found.",
-        };
-    }
+// export async function generateMetadata(props: { params: tParams }) {
+//     const { slug } = await props.params;
+//     const blogId = slug[1];
+//     const postRes = await fetch(`${api.blog.allBlogs}/${blogId}`, {
+//         cache: "no-store",
+//         headers: { "Content-Type": "application/json" },
+//     });
 
-    const postJson = await postRes.json();
-    const post: Blog = postJson.data;
+//     if (!postRes.ok) {
+//         return {
+//             title: "Blog Post Not Found",
+//             description: "The requested blog post could not be found.",
+//         };
+//     }
 
-    return {
-        title: `${post.title} | My Blog`,
-        description:
-            post.description || "A blog post about interesting topics.",
-        keywords: post.tags?.join(", ") || "blog, article, post",
-        openGraph: {
-            title: post.title,
-            description:
-                post.description || "A blog post about interesting topics.",
-            type: "article",
-            publishedTime: post.createdAt,
-            authors: post.author ? [post.author] : undefined,
-            tags: post.tags,
-            images: post.banner
-                ? [
-                      {
-                          url: post.banner,
-                          width: 1200,
-                          height: 630,
-                          alt: post.title,
-                      },
-                  ]
-                : undefined,
-        },
-        twitter: {
-            card: "summary_large_image",
-            title: post.title,
-            description:
-                post.description || "A blog post about interesting topics.",
-            images: post.banner ? [post.banner] : undefined,
-        },
-    };
-}
+//     const postJson = await postRes.json();
+//     const post: Blog = postJson.data;
+
+//     return {
+//         title: `${post.title} | My Blog`,
+//         description:
+//             post.description || "A blog post about interesting topics.",
+//         keywords: post.tags?.join(", ") || "blog, article, post",
+//         openGraph: {
+//             title: post.title,
+//             description:
+//                 post.description || "A blog post about interesting topics.",
+//             type: "article",
+//             publishedTime: post.createdAt,
+//             authors: post.author ? [post.author] : undefined,
+//             tags: post.tags,
+//             images: post.banner
+//                 ? [
+//                       {
+//                           url: post.banner,
+//                           width: 1200,
+//                           height: 630,
+//                           alt: post.title,
+//                       },
+//                   ]
+//                 : undefined,
+//         },
+//         twitter: {
+//             card: "summary_large_image",
+//             title: post.title,
+//             description:
+//                 post.description || "A blog post about interesting topics.",
+//             images: post.banner ? [post.banner] : undefined,
+//         },
+//     };
+// }
 
 export default async function BlogPostPage(props: { params: tParams }) {
     const { slug } = await props.params;
